1fbba656bd983bb60ce3241d35eac323de66d69c
craig.curtin <ccurtin@dsthealth.com>
2021-03-05 16:16:04 -0500

    update argument value in script

--- perf_test.sh
+++ perf_test.sh
@@ -12,17 +12,21 @@ rm -rf ${SAVE_TIME_ITERATION} 2> /dev/null
 
 source venv/bin/activate
 
-echo "main.py --config_tag fwa --base_record 0  --max_records 1000 --number_of_fetches 500" 
+echo "main.py --config_tag fwa --base_record 0  --max_records 10 --number_of_fetches 5000" 
 # redirect stdout to /dev/null so we don't count internal i/o in metrics
-time python3 main.py  --config_tag fwa --base_record 0  --max_records 100 --number_of_fetches 100 1> /dev/null | tee -a ${SAVE_TIME_ITERATION}
+time python3 main.py  --config_tag fwa --base_record 0  --max_records 10 	--number_of_fetches 5000 1> /dev/null | tee -a ${SAVE_TIME_ITERATION}
 
-echo "main.py --config_tag fwa --base_record 0  --max_records 5000 --number_of_fetches 100" 
+echo "main.py --config_tag fwa --base_record 0  --max_records 100 --number_of_fetches 500" 
 # redirect stdout to /dev/null so we don't count internal i/o in metrics
-time python3 main.py  --config_tag fwa --base_record 0  --max_records 100 --number_of_fetches 100 1> /dev/null | tee -a ${SAVE_TIME_ITERATION}
+time python3 main.py  --config_tag fwa --base_record 0  --max_records 100 	--number_of_fetches 500 1> /dev/null | tee -a ${SAVE_TIME_ITERATION}
 
-echo "main.py --config_tag fwa --base_record 0  --max_records 10000 --number_of_fetches 50" 
+echo "main.py --config_tag fwa --base_record 0  --max_records 1000 --number_of_fetches 50" 
 # redirect stdout to /dev/null so we don't count internal i/o in metrics
-time python3 main.py  --config_tag fwa --base_record 0  --max_records 100 --number_of_fetches 100 1> /dev/null | tee -a ${SAVE_TIME_ITERATION}l
+time python3 main.py  --config_tag fwa --base_record 0  --max_records 1000 	--number_of_fetches 50 	1> /dev/null | tee -a ${SAVE_TIME_ITERATION}
+
+echo "main.py --config_tag fwa --base_record 0  --max_records 1000 --number_of_fetches 5" 
+# redirect stdout to /dev/null so we don't count internal i/o in metrics
+time python3 main.py  --config_tag fwa --base_record 0  --max_records 10000 	--number_of_fetches 5 	1> /dev/null | tee -a ${SAVE_TIME_ITERATION}l
 
 cat ${SAVE_TIME_ITERATION}
 
