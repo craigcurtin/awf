27f824fc3331760b6c45aed52b62a1fc67472895
craig.curtin <ccurtin@dsthealth.com>
2021-04-05 10:54:21 -0400

    scripts and compiled python for Docker image, scripts run either compiled or uncompiled comparison. ....

---
+++ build_compiled.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+docker build --tag pyfwa_c --file Dockerfile.compiled  .
--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# below uses the default value if $1 is not passed as an argument ... 
 
-docker run -v /Users/dt230133:/root -v /var/log:/apps/log pyfwa --base_record 42 
+# set up the default image
+DOCKER_IMAGE="${1:-pyfwa}"
+
+docker run -v /Users/dt230133:/root -v /var/log:/apps/log ${DOCKER_IMAGE} --base_record 42 
